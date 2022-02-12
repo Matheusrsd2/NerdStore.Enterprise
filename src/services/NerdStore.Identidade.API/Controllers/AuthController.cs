024d5b2cbe99b1620e1370280045da602d856e49
matheusrsd2 <matttheus.reee@gmail.com>
2022-02-12 02:21:31 -0300

    Identity Auth

---
+++ src/services/NerdStore.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NerdStore.Identidade.API.Model;
+
+namespace NerdStore.Identidade.API.Controllers
+{
+    [Route("api/Identidade")]
+    public class AuthController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        //private readonly AppSettings _appSettings;
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager
+                              //IOptions<AppSettings> appSettings
+            )
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            //_appSettings = appSettings.Value;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+            return BadRequest(result);  
+        }
+
+        [HttpPost("Login")]
+        public async  Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+    }
+}
--- src/services/NerdStore.Identidade.API/Program.cs
+++ src/services/NerdStore.Identidade.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using NerdStore.Identidade.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "NerdStore Enterprise Identity API",
+        Description = "API Curso Desenvolvedor.io",
+        Contact = new OpenApiContact() { Name = "Matheus Reis" },
+        License = new OpenApiLicense() { Name = "MIT"}
+    });
+});
+
 
 var app = builder.Build();
 
@@ -16,8 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
